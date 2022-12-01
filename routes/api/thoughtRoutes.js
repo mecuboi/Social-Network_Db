73483003d7a0833dae4f800e867e35ed52eb4d6e
mecuboi <matthewd.jusuf@gmail.com>
2022-12-01 16:18:49 +1100

    adding virtuals to JSON

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -19,10 +19,10 @@ router
   .put(updateThought)
   .delete(deleteThought);
 
-// /api/thoughts/:thoughtId/reaction
-router.route('/:thoughtId/reaction').post(addReaction);
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
 
-// /api/thoughts/:thoughtId/reaction/:reactionId
-router.route('/:thoughtId/reaction/:reactionId').delete(deleteReaction);
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
